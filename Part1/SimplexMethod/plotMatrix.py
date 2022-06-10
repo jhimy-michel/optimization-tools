451e87cfe66530c50100ea74385da5343b707cdc
jhimy-michel <jhimy0riv@gmail.com>
2022-06-10 09:12:45 +0200

    testing sympy plot lib

--- Part1/SimplexMethod/plotMatrix.py
+++ Part1/SimplexMethod/plotMatrix.py
@@ -1,8 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
 """ Work in progress, 
 the idea is to receive the matrix A and B and plot a graph """
+colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
+A = np.array([
+    [-1, -2],  # vector 1
+    [-1, 1],  # vector 2
+    [1, 0],  # vector 3
+    [0, 1],  # vector 4
+    [0, -1],  # vector 5
+])
 
 
-""" plt.figure(figsize=(8, 6))
+b = np.array([
+    [-2],
+    [1],
+    [3],
+    [2],
+    [0]
+])
+x1 = np.linspace(0, 5, 100)
+plt.figure(figsize=(8, 6))
 for i in range(len(A)):
     div = A[i][1] if A[i][1] > 0 else 1
     
@@ -11,8 +29,8 @@ for i in range(len(A)):
         plt.plot(x1, x2, colors[i], label='x2 = ( x1 * {0} + {1} ) / {2}'.format(
             A[i][0], b[i][0], div), linewidth=2.0)
     else:
-        x2 = xaux * 0 + b[i][0]
-        plt.plot(x2, xaux, colors[i], label='x1 =  x2 * {0} + {1} '.format(
+        x2 = x1 * 0 + b[i][0]
+        plt.plot(x2, x1, colors[i], label='x1 =  x2 * {0} + {1} '.format(
             0, b[i][0]), linewidth=2.0)
 
 
@@ -23,4 +41,4 @@ plt.legend(loc='upper left')
 
 
 plt.grid()
-plt.show() """
+plt.show()
